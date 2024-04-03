92acfc5baf4228cb7a31bac9864f9525d6fc696d
Lex <vasickin@mail.ru>
2024-04-03 23:10:17 +0300

    Convert to single hot coding without using get_dummies and loops

--- df.py
+++ df.py
@@ -6,3 +6,4 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 
+one_hot_encoded = pd.concat([pd.Series(1, index=data.index, name='whoAmI_'+i) if i == data['whoAmI'].iloc[j] else pd.Series(0, index=data.index, name='whoAmI_'+i) for j, i in enumerate(set(data['whoAmI']))], axis=1)
